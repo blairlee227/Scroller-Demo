faf1fb93950380aaa562f1d08995280bb87b782f
Blair <31032281+blairlee227@users.noreply.github.com>
2022-03-23 13:39:03 +0800

    Create lazy loading demo

---
+++ lazy-loading/lazy-loading.js
@@ -0,0 +1,49 @@
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random/9');
+    const data = await response.json();
+    return data?.message;
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const createdDom = (link) => {
+  const box = document.createElement('div')
+  box.setAttribute("class", "box");
+
+  const img = document.createElement('img')
+  img.setAttribute("src", link);
+  box.appendChild(img)
+
+  return box
+}
+
+const containerEl = document.getElementById('container')
+const render = (renderData) => {
+  const fragment = document.createDocumentFragment();
+
+  for (const data of renderData) {
+    const box = createdDom(data)
+    fragment.appendChild(box)
+  }
+
+  containerEl.appendChild(fragment);
+}
+
+
+// Intersection Observer ****************************************************
+const targetEl = document.getElementById('observeTarget');
+let intersectionObserver = new IntersectionObserver(async (entries) => {
+  // intersectionRatio: 目前元素可見的比例
+  // 0 代表不可見，1 代表可見
+  if (entries[0].intersectionRatio <= 0) return;
+
+  let renderData = [];
+  renderData = await fetchData();
+  render(renderData);
+});
+
+// start observing
+intersectionObserver.observe(targetEl);
+// Intersection Observer ****************************************************
